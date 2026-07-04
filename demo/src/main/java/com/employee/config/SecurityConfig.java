888bb0cd99349f60cddb51b6f2b2cc8b8762c569
Akash <akash.aravind28@gmail.com>
2026-07-04 18:16:56 +0530

    New Boot Test

--- demo/src/main/java/com/employee/config/SecurityConfig.java
+++ demo/src/main/java/com/employee/config/SecurityConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.oauth2.server.resource.web.DefaultBearerTokenResolver;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -12,11 +13,13 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
+        DefaultBearerTokenResolver resolver= new DefaultBearerTokenResolver();
+        resolver.setBearerTokenHeaderName("X-AZURE-TOKEN");
         http.authorizeHttpRequests(auth-> auth
                 .requestMatchers("/employee/**").authenticated().
                 requestMatchers("/h2-console/**").permitAll().anyRequest().authenticated())
                 .oauth2ResourceServer(oauth2->
-                        oauth2.jwt(jwt->{}));
+                        oauth2.bearerTokenResolver(resolver).jwt(jwt->{}));
 
         http.csrf(csrf->csrf.disable());
         http.headers(headers->headers.frameOptions(frame-> frame.disable()));
